24a33436c205c2a6f65564f3aa9d6aa07cb88470
nurasyl <nurasyl1999j@gmail.com>
2025-02-26 23:37:08 +0600

    3-lesson Create EmployeeDto and Create EmployeeMapper

---
+++ src/main/java/org/example/springbootpractice2/mapper/EmployeeMapper.java
@@ -0,0 +1,25 @@
+package org.example.springbootpractice2.mapper;
+
+import org.example.springbootpractice2.dto.EmployeeDto;
+import org.example.springbootpractice2.entity.Employee;
+
+public class EmployeeMapper {
+
+    public EmployeeDto toEmployeeDto(Employee employee) {
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+
+    public Employee toEmployee(EmployeeDto employeeDto) {
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+}
